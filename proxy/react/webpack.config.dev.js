418a81116cd796b775c2f1c5429d8f2a9ef98c60
terut <terut.dev+github@gmail.com>
2018-03-15 03:15:53 +0900

    Add proxy

---
+++ proxy/react/webpack.config.dev.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/Index.tsx',
+  output: {
+    filename: './react-bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: 'ts-loader'}
+        ]
+      }
+    ]
+  }
+};
