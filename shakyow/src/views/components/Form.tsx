da983b68f72fa9784105770b91965859cb90f448
terut <terut.dev+github@gmail.com>
2019-08-31 17:37:56 +0900

    Add useState of hooks

--- shakyow/src/views/components/Form.tsx
+++ shakyow/src/views/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps, withRouter, Link } from 'react-router-dom';
 import './Form.css';
 
@@ -9,12 +9,24 @@ type Props = {
 const _form: React.FC<Props> = (props: Props) => {
   const { addSutra } = props
 
+  const [url, setURL] = useState("")
+  const [description, setDescription] = useState("")
+
+  const handleURLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setURL(e.target.value)
+  }
+
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(e.target.value)
+  }
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
-    console.log("submit!!!")
+    console.log("submit!!!", url, description)
+
     addSutra({
-      url: "https://github.com/terut/duo-desktop",
-      description: "good example",
+      url: url,
+      description:  description,
     })
   }
 
@@ -26,11 +38,11 @@ const _form: React.FC<Props> = (props: Props) => {
         <form onSubmit={handleSubmit}>
           <div className="form-text">
             <label htmlFor="url">Example URL</label>
-            <input type="text" name="url" required />
+            <input type="text" name="url" value={url} onChange={handleURLChange} required />
           </div>
           <div className="form-textarea">
             <label htmlFor="description">Description <span className="form-note">(optional)</span></label>
-            <textarea></textarea>
+            <textarea value={description} onChange={handleDescriptionChange}></textarea>
           </div>
           <div className="form-button">
             <input className="button-primary" type="submit" value="Post" />
